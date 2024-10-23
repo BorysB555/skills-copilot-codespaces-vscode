79766f2d2090b6d08a745e905731f1cedacd8f84
Borys Bessarab <borys_bessarab0608@tntu.edu.ua>
2024-10-23 21:15:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,34 @@
 //create web server
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const commentsPath = path.join(__dirname, 'comments.json');
+const comments = require(commentsPath);
+const port = 3000;
 
-//create route
-app.get('/comments', function(req, res) {
-    res.send('This is the comments page!');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.get('/comments', (req, res) => {
+  res.json(comments);
 });
 
-//start server
-app.listen(8080);
-console.log('Server started on port 8080');
+app.post('/comments', (req, res) => {
+  const newComment = req.body;
+  newComment.id = comments.length + 1;
+  comments.push(newComment);
+
+  fs.writeFile(commentsPath, JSON.stringify(comments), (err) => {
+    if (err) {
+      res.status(500).send('An error occurred');
+    } else {
+      res.json(newComment);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
